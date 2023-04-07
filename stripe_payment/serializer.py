ffd63908c1818e5266d4bf090544af7ac9428b95
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 22:46:49 +0330

    add SetupIntent serializer

--- stripe_payment/serializer.py
+++ stripe_payment/serializer.py
@@ -6,3 +6,14 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = "__all__"
+
+
+class SetupIntentSerializer(serializers.Serializer):
+
+    desc = serializers.CharField(error_messages = {
+        "required" : "Please Enter Describtion",
+    })
+
+    payment_method_types = serializers.ListField(error_messages = {
+        "required" : "Please Enter Payment Method Types"
+    })

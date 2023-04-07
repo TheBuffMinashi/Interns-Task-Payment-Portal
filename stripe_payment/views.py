c9a26dbd1be86541137ba2a2f7cf5e3a1c1c0bc1
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 22:22:39 +0330

    complet getting information for adding new Customer

--- stripe_payment/views.py
+++ stripe_payment/views.py
@@ -1,3 +1,35 @@
+from typing import Dict,Any
+
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .serializer import CustomerSerializer
+from .models import Customer
+
+
+class NewCustomer(APIView):
+
+    context:Dict[str,Any]
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self,request):
+
+        customer_serializer = CustomerSerializer(request.data)
+
+        if customer_serializer.is_valid():
+
+            customer_serializer.save()
+
+            self.context = {
+                "message" : "Customer Added Successfully ..."
+            }
+
+            return Response(self.context,status=status.HTTP_200_OK)
+        
+        return Response(customer_serializer.error_messages,status=status.HTTP_400_BAD_REQUEST)

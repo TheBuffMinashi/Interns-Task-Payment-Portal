d11b8bd6a1f9d5e0e1815a76d1dd35381a03788f
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 20:56:11 +0330

    create StripeInformation model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class StripeInformation(models.Model):
+
+    user = models.OneToOneField(
+        User,on_delete=models.CASCADE,verbose_name="User"
+    )
+
+    stripe_id = models.CharField(max_length=50,
+                                 verbose_name="Stripe Id",
+                                 unique=True,
+                                 blank=True,
+                                 null=True)
+    
+    def __str__(self) -> str:
+        return f"{self.user.username} --- {self.stripe_id}"
+    
+    class Meta:
+        verbose_name:str = "Stripe Information"
+        verbose_name_plural:str = "Stripe Information"

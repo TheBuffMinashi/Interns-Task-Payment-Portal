57aab7692f88b89674f6b97bc24e85d4c6247cda
Mohammadreza <mohammadreza.mrn.29@gmail.com>
2023-04-07 23:17:56 +0330

    create models related to stripe payment system

--- stripe_payment/models.py
+++ stripe_payment/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+
+    email = models.EmailField(
+        max_length=150,verbose_name="Email"
+    )
+
+    describtion = models.TextField(
+        max_length=500,verbose_name="Desc"
+    )
+
+    name = models.CharField(max_length=50,
+                            verbose_name="Name")
+    
+    phone = models.CharField(max_length=15,
+                             verbose_name="Phone")
+    
+
+    def __str__(self) -> str:
+        return self.email
+    
+    class Meta:
+        verbose_name_plural = "Customers"
+
+
+class SetupIntent(models.Model):
+
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE,verbose_name="From Customer",related_name="Customer",null=True)
+
+    desc = models.TextField(verbose_name="Describtion")
+
+    def __str__(self) -> str:
+        return self.customer.name
+    
+    class Meta:
+        verbose_name_plural = "SetupIntent"

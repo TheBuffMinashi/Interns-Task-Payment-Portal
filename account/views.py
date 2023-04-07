fc32c081746eed9f438bedb567447a071d266e19
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 21:06:40 +0330

    create RegisterView for registering

--- account/views.py
+++ account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+
+
+class RegisterUser(APIView):
+
+    def post(self,request):
+        ...

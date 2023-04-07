28114230d02ad301d64898d6571bc72b5398cda0
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 21:53:08 +0330

    create class for FileAttaching like passport for identity

--- stripe_payment/payment/payment.py
+++ stripe_payment/payment/payment.py
@@ -1,3 +1,5 @@
+from typing import Any,Dict
+
 import stripe
 
 from .constant import STRIPE_KEY
@@ -41,3 +43,47 @@ class Customer:
             name = self.__name,
             phone = self.__phone
         )
+
+
+class FileAttach:
+
+    def __init__(self,**information) -> None:
+        
+        self.__purpose:str = information["purpose"]
+        self.__file = information["file"]
+        self.__stripe_account:str = information["account"]
+
+    @property
+    def purpose(self) -> str:
+        return self.__purpose
+    
+    @property
+    def file(self):
+        return self.__file
+    
+    @property
+    def stripe_account(self) -> str:
+        """
+
+        Returns:
+            str: stripe id returning
+        """
+
+        return self.__stripe_account
+    
+    def attach_new(self) -> Any:
+
+        file_attaching_account = stripe.File.create(
+            purpose = self.__purpose,
+            file = self.__file,
+            stripe_account = self.__stripe_account
+        )
+
+        individual:Dict[str,Any] = {
+            "verification" : {
+                "document" : {"front" : file_attaching_account.get("id"),},
+                "additional_document" : {"front" : file_attaching_account.get("id")},
+            }
+        }
+
+        stripe.Account.modify(self.__stripe_account,individual = individual)

2ad9cffb6a439f7fdac5b2a09a349e1a9900fa5f
sajjad fani <faniam321@gmail.com>
2023-04-07 17:45:21 +0430

    migrate user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField(_("Email address"), unique=True)
+    full_name = models.CharField(_('Fullname'), max_length=50)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['full_name']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email

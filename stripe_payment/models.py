41a198caa8bd46eb0cd5e4cf9b03411ed0b70f77
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 22:17:00 +0330

    create Customer model

--- stripe_payment/models.py
+++ stripe_payment/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+
+    email = models.EmailField(
+        max_length=150,verbose_name="Email"
+    )
+
+    describtion = models.TextField(
+        max_length=500,verbose_name="Desc"
+    )
+
+    name = models.CharField(max_length=50,
+                            verbose_name="Name")
+    
+    phone = models.CharField(max_length=15,
+                             verbose_name="Phone")
+    
+
+    def __str__(self) -> str:
+        return self.email
+    
+    class Meta:
+        verbose_name_plural = "Customers"

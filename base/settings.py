29a4de78c6c3c59ba16514ad18fe8bf3ed37f84c
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 19:35:04 +0330

    config settings for connectin to mysql database

--- base/settings.py
+++ base/settings.py
@@ -82,6 +82,20 @@ WSGI_APPLICATION = 'base.wsgi.application'
 # }
 
 
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'stripe_payment',  
+        'USER': 'root',  
+        'PASSWORD': 'admin',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

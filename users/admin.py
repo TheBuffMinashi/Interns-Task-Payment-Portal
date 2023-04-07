be80d362a1e246ee8dea0640cd865d4e3fad79d5
sajjad fani <faniam321@gmail.com>
2023-04-07 18:47:37 +0430

    complete admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('email', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        ('Personal Information', {
+         'fields': ('email', 'full_name', 'password')}),
+        ("Permissions",
+            {'fields': (
+                'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        ('Personal Information', {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'full_name', 'password1', 'password2',
+            )}),
+        ('Permissions', {
+            'classes': ('wide',),
+            'fields':
+            ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')})
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)

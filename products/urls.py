89095bc06514b55423de6915601ec5aad38d7daa
sajjad fani <faniam321@gmail.com>
2023-04-07 21:07:04 +0430

    complete product app

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import (
+    CreateProduct, ProductList, RetrieveUpdateDestroyProduct,
+)
 
 app_name = 'products'
 
 urlpatterns = [
-
+    path('create/', CreateProduct.as_view(), name='create-product'),
+    path('list/', ProductList.as_view(), name='product-list'),
+    path('list/<int:pk>/', RetrieveUpdateDestroyProduct.as_view(),
+         name='retrieve-update-destroy-product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
+from .models import Product
+from .serializers import (
+    ProductSerializer,
+)
 
-# Create your views here.
+
+class CreateProduct(CreateAPIView):
+    permission_classes = (IsAdminUser,)
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class ProductList(ListAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+class RetrieveUpdateDestroyProduct(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAdminUser,)
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()

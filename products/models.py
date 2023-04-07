5b4f4a3c297eaa4f25dd6f023831ac37cd81944f
sajjad fani <faniam321@gmail.com>
2023-04-07 20:59:19 +0430

    add new model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(
+        verbose_name=_('Description'), null=True, blank=True)
+    image = models.ImageField(
+        upload_to='products/images/', verbose_name=_('Image'), null=True, blank=True)
+    price = models.IntegerField(verbose_name=_('Price'), default=0)
+
+    def __str__(self) -> str:
+        return self.name

84a089e65e0a699566d794468d0ab4572cf55bdc
sajjad fani <faniam321@gmail.com>
2023-04-07 22:00:39 +0430

    checkout stripe

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import (
+    CreateCheckoutSessionSerializer,
+)
 
-# Create your views here.
+
+class CreateCheckoutSession(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        serializer = CreateCheckoutSessionSerializer(
+            data=request.data, context={'request': request, 'pk': pk}
+        )
+        if serializer.is_valid(raise_exception=True):
+            data = serializer.save()
+            if data is not False:
+                return Response(status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_400_BAD_REQUEST)

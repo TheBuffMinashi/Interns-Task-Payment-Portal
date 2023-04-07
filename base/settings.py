92072d8ae23b28cfd19341f342484234022212ed
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-07 20:20:45 +0330

    add account app to installed apps in base app

--- base/settings.py
+++ base/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "stripe_payment.apps.StripePaymentConfig",
+    "account.apps.AccountConfig",
 ]
 
 MIDDLEWARE = [

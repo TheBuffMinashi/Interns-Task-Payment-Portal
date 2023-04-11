81ba32534703a643ebbb1a2d6b0c1afae9abfcce
Mohammad Amin <mohammadaminmiri22@gmail.com>
2023-04-11 22:05:51 +0330

    Added APIs for creating a payment and confirming API status

---
+++ payment/serializers.py
@@ -0,0 +1,34 @@
+import stripe
+
+from rest_framework import serializers
+from rest_framework.exceptions import APIException
+
+from payment.models import Payment
+
+
+class CreatePaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = ['amount', 'currency', 'client_secret', 'status']
+        read_only_fields = ['client_secret', 'status']
+
+    def create(self, validated_data):
+        payment = Payment(**validated_data)
+        try:
+            payment_intent = stripe.PaymentIntent.create(
+                amount=payment.amount,
+                currency=validated_data.get('currency'),
+                # payment_method_types=['card'],
+                automatic_payment_methods={
+                    "enabled": True
+                },
+            )
+        except stripe.error.StripeError as e:
+            print(e.user_message)
+            raise APIException
+        else:
+            payment.payment_id = payment_intent.id
+            payment.client_secret = payment_intent.client_secret
+            payment.status = Payment.PaymentStatus.PENDING
+            payment.save()
+        return payment
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Payment
+from .serializers import CreatePaymentSerializer
+
+
+class CreatePaymentAPIView(CreateAPIView):
+    serializer_class = CreatePaymentSerializer
+    permission_classes = [AllowAny]
+
+
+class ConfirmPaymentAPIView(APIView):
+    def post(self, request):
+        data = self.request.data
+        payment_id = data.get('data').get('object').get('id')
+        try:
+            payment = Payment.objects.get(payment_id=payment_id)
+        except Payment.DoesNotExist:
+            raise ValidationError(detail='Invalid payment id')
+        else:
+            payment_type = data.get('type')
+            if payment_type == 'payment_intent.succeeded':
+                payment.status = payment.PaymentStatus.DONE
+            elif payment_type == 'payment_intent.payment_failed':
+                payment.status = payment.PaymentStatus.FAILED
+            payment.date_modified = timezone.now()
+            payment.save()
+            response_data = {'detail': 'event received'}
+            return Response(response_data, status=status.HTTP_200_OK)

c50f9f1e5f57efdb20a9c8aeacbc43782c2f38ce
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-12 15:51:09 +0330

    create StripePayment model

--- stripe_payment/models.py
+++ stripe_payment/models.py
@@ -1,3 +1,5 @@
+import json
+
 from django.db import models
 
 class Customer(models.Model):
@@ -35,3 +37,23 @@ class SetupIntent(models.Model):
     
     class Meta:
         verbose_name_plural = "SetupIntent"
+
+
+class PaymentIntentModel(models.Model):
+
+    customer = models.ForeignKey(Customer,verbose_name="Customer",on_delete=models.CASCADE)
+    describtion = models.TextField(verbose_name="Describtion")
+    amount = models.PositiveBigIntegerField(verbose_name="Amount ")
+    currency = models.CharField(max_length=20,verbose_name="Currency",default="pln")
+    payment_method_types = models.CharField(max_length=500)
+    capture_method = models.CharField(default="manual",max_length=100)
+
+    @property
+    def payment_method(self):
+        return json.loads(self.payment_method_types)
+    
+    @payment_method.setter
+    def payment_method(self,value):
+        self.payment_method_types = json.dumps(value)
+
+

cc944b3a450b46eb1c2a46a334d17784fbdcabcb
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-13 01:06:17 +0330

    create SetupIntentTestCase class

--- stripe_payment/tests.py
+++ stripe_payment/tests.py
@@ -3,34 +3,40 @@ from typing import Dict,Any
 from django.test import TestCase,Client
 
 from . import views
+from .models import Customer
 
-class NewCustomerTestCase(TestCase):
 
-    __SUCCESS_CODE:int = 200
+class StripeSystemTestCase(TestCase):
+
+    _SUCCESS_CODE:int = 200
 
     def setUp(self) -> None:
         
-        self.__client = Client(enforce_csrf_checks=True)
+        self._client = Client(enforce_csrf_checks=True)
         
         self.__test_username:str = "admin"
         self.__test_password:str = "admin"
 
         self.__token_key:str = "f3247bc65d56855050ce63e9a1cfd36c68159402"
 
-        state:bool = self.__client.login(
+        state:bool = self._client.login(
             username = self.__test_username,
             password = self.__test_password
         )
 
-        print(state)
-
-        self.__header:Dict[str,Any] = {
+        self._header:Dict[str,Any] = {
             "Authorization" : f"Token {self.__token_key}"
         }
+        
 
-        self.__url:str = "/new-customer"
+
+class NewCustomerTestCase(StripeSystemTestCase):
+
 
     def test_new_customer(self):
+
+        url:str = "/new-customer"
+
 
         data:Dict[str,Any] = {
             "email" : "test@gmail.com",
@@ -39,9 +45,39 @@ class NewCustomerTestCase(TestCase):
             "phone" : "09123456789",
         }
 
-        result = self.__client.post(self.__url,data,**self.__header)
+        result = self._client.post(url,data,**self._header)
 
-        print(result.content)
 
         self.assertEqual(result.status_code,self.__SUCCESS_CODE)
 
+
+class SetupIntentTestCase(StripeSystemTestCase):
+
+    def test_new_setup_intent(self):
+
+        url:str = "/setup-intent"
+
+        customer_data:Dict[str,Any] = {
+            "email" : "test@gmail.com",
+            "describtion" : "test desc",
+            "name" : "example name",
+            "phone" : "09123456789",
+        }
+
+        customer = Customer.objects.create(
+            **customer_data
+        )
+
+        data:Dict[str,Any] = {
+            "desc" : "Test Describtion",
+            "payment_method_types" : ["a","b"],
+            "customer_id" : customer.id,
+        }
+
+        response = self._client.post(
+            url,data = data,**self._header
+        )
+
+        self.assertEqual(response.status_code,self._SUCCESS_CODE)
+
+        

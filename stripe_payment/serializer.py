987d95339f789fab50d5925b3dcbb4cea6eebfa3
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-04-12 19:50:52 +0330

    fix create method of PaymentIntent serializer

--- stripe_payment/serializer.py
+++ stripe_payment/serializer.py
@@ -2,9 +2,9 @@ from typing import Dict,Any
 
 from rest_framework import serializers
 
-from .models import Customer
+from .models import Customer,PaymentIntentModel
 from .models import SetupIntent as SetupIntentModel
-from .payment.payment import SetupIntent
+from .payment.payment import SetupIntent,PaymentIntent
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -56,7 +56,7 @@ class SetupIntentSerializer(serializers.Serializer):
         return setup_intent
 
 
-class PaymentIntentSerializer:
+class PaymentIntentSerializer(serializers.Serializer):
 
     describtion = serializers.CharField(
         error_messages = {
@@ -66,9 +66,10 @@ class PaymentIntentSerializer:
 
     customer = serializers.IntegerField(
         default = 0,
-        error_messages = {
-            "required" : "Please Enter Customer"
-        }
+    )
+
+    currency = serializers.CharField(
+        default = "pln"
     )
 
     amount = serializers.IntegerField(
@@ -86,5 +87,48 @@ class PaymentIntentSerializer:
     capture_method = serializers.CharField(
         default = "manual"
     )
-    
+
+
+    def create(self, validated_data:Dict[str,Any]):
+        
+        customer_id:int = validated_data.get("customer",0)
+
+        describtion:str = validated_data.get("describtion")
+
+        amount:int = validated_data.get("amount")
+
+        payment_method_types = validated_data.get("payment_method_types")
+
+        currency = validated_data.get("currency")
+        
+        capture_method = validated_data.get("capture_method")
+
+        if not customer_id:
+            raise serializers.ValidationError("Customer Id is not Valid ... !")
+        
+        customer = Customer.objects.filter(id = customer_id).first()
+
+        if not customer:
+            raise serializers.ValidationError("Customer Not Found ... !")
+        
+        payment_intent_model = PaymentIntentModel(
+            customer = customer,
+            describtion = describtion,
+            amount = amount,
+            capture_method = capture_method,
+            currency = currency
+        )
+        payment_intent_model.payment_method = payment_method_types
+
+
+        PaymentIntent(
+            describtion = describtion,
+            customer = customer,
+            amount = amount,
+            currency = currency,
+            payment_method_types = payment_method_types,
+            capture_method = capture_method
+        ).new_pay()
+
+
         

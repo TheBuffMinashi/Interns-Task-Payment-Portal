c26525d177d58f8fdf4b25bbcaca125808887a8d
Hamidreza Ebrahimi <hamidrezaebrahimi67@gmail.com>
2023-04-20 00:24:38 +0330

    Connect with Stripe and pay add new file

---
+++ pay/static/pay/main.js
@@ -0,0 +1,25 @@
+console.log("Sanity check!");
+
+// Get Stripe publishable key
+fetch("/config/")
+.then((result) => { return result.json(); })
+.then((data) => {
+  // Initialize Stripe.js
+  const stripe = Stripe(data.publicKey);
+  console.log(stripe);
+
+  // Event handler
+  document.querySelector("#checkout-button").addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/pay/")
+    .then((result) => { return result.json(); })
+    .then((data) => {
+      console.log(data);
+      // Redirect to Stripe Checkout
+      return stripe.redirectToCheckout({sessionId: data.sessionId})
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  });
+});
